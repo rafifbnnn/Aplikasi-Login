de551c3768f48b7ab4a1b93005ef1e7704c95b8f
rafifbnnn <91790563+rafifbnnn@users.noreply.github.com>
2024-02-18 22:32:07 +0700

    Update login-script.js
    Menambahkan komentar dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,21 +5,58 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/* Membuat variabel untuk setiap elemen view */
+
+/** @module Login-Script */
+/** 
+* Membuat variabel loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+/** 
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+/** 
+* Membuat variabel inputPasswordElement untuk tampilan input password.
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/* Membuat variabel untuk menyimpan informasi email dan password */
+/** 
+ * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
+ * @constant {string}
+ */
 const expectedEmail = 'admin@dicoding.com';
+/** 
+ * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
+ * @constant {string}
+ */
 const expectedPassword = 'superpassword';
 
+/* Menambahkan aksi klik pada button */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /* Mendapatkan nilai dari masing-masing input nilai input (email & password) ketika tombol ditekan */
+  /** 
+   * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
+   * @constant {string}
+   */
   const email = inputEmailElement.value;
+  /** 
+   * Membuat variabel password untuk menyimpan niali password yang didapatkan saat button ditekan.
+   * @constant {string}
+   */
   const password = inputPasswordElement.value;
+  /* Memastikan bahwa nilai email & password sesuai dengan nilai yang tersimpan */
   if (email == expectedEmail && password == expectedPassword) {
+    /* Jika sesuai maka program akan berpindah ke halaman home */
     goToHome();
   } else {
+    /* Jika tidak sesuai maka akan menampilkan informasi bahwa input salah */
     showPopUp();
   }
 });
